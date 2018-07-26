db7b082bc27be99986e9bd6b81a4ffd627af0032
momo1627 <momo1627@gmail.com>
2018-07-26 20:45:20 +1000

    task14

---
+++ 14/nav_bar.js
@@ -0,0 +1,9 @@
+function navButton(){
+    var x = document.getElementById("nav_collapse");
+    if (x.style.display === "none"){document.getElementById("nav_collapse").style.display = "block"
+    }
+    else {document.getElementById("nav_collapse").style.display = "none"
+    }
+}/**
+ * Created by momo_ on 2018/7/23.
+ */
